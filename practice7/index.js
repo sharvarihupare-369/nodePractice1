b263e8f0e8f3b593aa6c8e740eb21a3ccbfbf87d
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-10-28 23:19:19 +0530

    Ad

---
+++ practice7/index.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const fs = require("fs")
+const server = http.createServer((req,res)=>{
+
+    if(req.url === "/" && req.method === "GET"){
+        res.end("This is home page")
+    }else if(req.url === "/data" && req.method === "GET"){
+        // res.end("data will be sent")
+        // fs.readFile("./data.json","utf-8",(err,data)=>{
+        //     if(err) console.log(err)
+        //     else res.end(data)
+        // })
+        fs.createReadStream("./data.json").pipe(res)
+    }else if(req.url == "/blog" && req.method==="GET"){
+        res.setHeader("Content-type","text/html")
+        res.write("<h1>Blogs</h1>")
+        res.end()
+    }else if(req.url == "/todos" && req.method == "GET"){
+        fs.readFile("./todo.json","utf-8",(err,data)=>{
+            if(err) res.end(err)
+            else res.end(data)
+        })
+    }else if(req.url === "/todos" && req.method === "POST"){
+        // let data = fs.createReadStream("./todo.json","utf-8")
+        // let dataStream = data.pipe(res)
+        let storeData = "";
+         
+        req.on('data',(chunk)=>{
+            storeData += chunk;
+        })
+       
+        req.on('end',()=>{
+            // console.log(storeData)
+            try {
+                let parsedTodo = JSON.parse(storeData)
+                let todos = []
+                fs.readFile("./todo.json","utf-8",(err,data)=>{
+                    if(err) res.end(err)
+                    else {
+                     todos = JSON.parse(data);
+                     todos.push(parsedTodo);  
+
+                    }
+                    fs.writeFile("./todo.json",JSON.stringify(todos),(err)=>{
+                        if(err) res.end(err)
+                        else res.end("Data added")
+                    })
+                })
+
+            } catch (error) {
+                res.end(error)
+            }
+        })
+        res.end("Data added")
+    }
+    else{
+        // res.end("Invalid endpoint")
+        res.end(http.STATUS_CODES["404"])
+    }
+
+})
+
+server.listen(5000,()=>{
+    console.log("Server is listening on port 5000")
+})
---
+++ pratice7exp/index.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const fs = require("fs")
+const app = express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+  res.send("Welcome to home page")
+})
+
+app.get("/data",(req,res)=>{
+  const dataStream = fs.createReadStream("./data.json","utf-8").pipe(res)
+})
+
+app.post("/adddata",(req,res)=>{
+  console.log(req.body)
+  res.send("Data has been sent")
+})
+
+app.get("/students",(req,res)=>{
+  let data =  fs.readFileSync("./data.json","utf-8")
+  const parsedData = JSON.parse(data)
+  // console.log(parsedData.students)
+  // res.send(JSON.stringify(parsedData.students))
+  res.send(parsedData.students)
+
+})
+
+app.post("/addstudent",(req,res)=>{
+  // Get complete data 
+  let data =  fs.readFileSync("./data.json","utf-8")
+
+  //Parse the data 
+  let parsedData = JSON.parse(data)
+  
+  // Add the data in students
+   parsedData.students.push(req.body)
+
+   fs.writeFileSync("./data.json",JSON.stringify(parsedData))
+
+   res.send("data added")
+
+  
+})
+
+app.delete("/deletestudent/:name",(req,res)=>{
+  let name = req.params.name
+  let data = fs.readFileSync("./data.json","utf-8")
+  let parsedData = JSON.parse(data)
+  const newStudentData = parsedData.students.filter(el=> el.name != name)
+  // console.log(newStudentData)
+  parsedData.students = newStudentData
+  fs.writeFileSync("./data.json",JSON.stringify(parsedData));
+  res.end("Student Deleted")
+})
+
+app.listen(5000,()=>{
+  console.log("Server is running on port 5000")
+})
